6a749b17d121359c0f4f686738a758f731edef85
giteejdw2005 <3472432052@qq.com>
2024-12-04 21:53:13 +0800

    1

--- 11-30-2.py
+++ 11-30-2.py
@@ -23,16 +23,41 @@
 # 请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
 # 如果找到请以按从小到大顺序返回答案。
 # 如果不存在请返回NULL
-nums=(input()).split(" ")
-target=int(input())
-a=0
-for i in range(len(nums)):
-    for k in range(i+1,len(nums)):
-        if target==int(nums[i])+int(nums[k]):
-            print(f"{i} {k}")
-            a=a+1
-if a==0:
-    print("NULL")
+# nums=(input()).split(" ")
+# target=int(input())
+# a=0
+# for i in range(len(nums)-1):
+#     if nums[i]=="":
+#         continue
+#     for k in range(i+1,len(nums)):
+#         if nums[k]=="":
+#             continue
+#         if target==int(nums[i])+int(nums[k]):
+#             print(f"{i} {k}")
+#             a=1
+        
+# if a==0:
+#     print("NULL")
 
-        
-        
+#   题目描述: 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串的长度。
+# 输入: 字符串s
+# 输出: 无重复字符的最长字串长度（整数)
+
+
+
+def sub(s:str)->str:
+    count={char:s.count(char) for char in set(s)}
+    in_s={}
+    stack=[]
+    for char in s:
+        count[char]-=1 
+        if in_s.get(char,False):
+            continue
+        while stack and stack[-1]> char and count[stack[-1]]>0:
+            r_char=stack.pop()
+            in_s[r_char]=False
+        stack.append(char)
+        in_s[char]=True
+    return ".join(stack)
+s=input()
+print(sub(s))
---
+++ 12-1.py
@@ -0,0 +1,6 @@
+a=[1,2,3]
+b=a
+a.append(4)
+b=b+[5]
+print(a)
+print(b)
